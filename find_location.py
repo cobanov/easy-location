8e4f08759d84f821e4aa99d910a065e96751aa88
Mert Cobanov <mertcobanov@gmail.com>
2022-07-05 00:23:01 +0300

    Create find_location.py

---
+++ find_location.py
@@ -0,0 +1,9 @@
+from geopy.geocoders import Nominatim
+from tqdm import tqdm
+from geopy.extra.rate_limiter import RateLimiter
+
+geolocator = Nominatim(user_agent="cobanov")
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+
+tqdm.pandas()
+df['gcode'] = df['full_adress'].progress_apply(geocode)
